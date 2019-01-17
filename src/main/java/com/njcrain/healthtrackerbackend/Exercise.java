1df54d81e467701f8392115cce95788ba1c8abe7
Nick Crain <nicholascrain@gmail.com>
2019-01-17 11:24:01 -0800

    made Exercise class work better with hibernate

--- src/main/java/com/njcrain/healthtrackerbackend/Exercise.java
+++ src/main/java/com/njcrain/healthtrackerbackend/Exercise.java
@@ -10,19 +10,53 @@ public class Exercise {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     public long id;
-    public String title;
-    public int quantity;
-    public String description;
-    public String timestamp;
+    private String title;
+    private int quantity;
+    private String description;
+    private String timestamp;
 
-    public Exercise(String title, int quantity, String description, String timestamp) {
-        this.title = title;
-        this.quantity = quantity;
-        this.description = description;
-        this.timestamp = timestamp;
-    }
+    public Exercise() {}
+//
+//    public Exercise(String title, int quantity, String description, String timestamp) {
+//        this.title = title;
+//        this.quantity = quantity;
+//        this.description = description;
+//        this.timestamp = timestamp;
+//    }
 
     public String toString() {
         return this.title + ": " + this.description + " - " + this.timestamp;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getTimestamp() {
+        return this.timestamp;
     }
 }
